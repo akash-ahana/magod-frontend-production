35998c263e659a2736ba49c39ffc7aefdbd8d877
Prakruthi Holla <prakruthi.holla@ahana.co.in>
2023-07-10 12:35:16 +0530

    login changes

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import "./Login.css"
 import Logo from "../Auth/Magod Laser Logo - Default [2500].png"
 import { useNavigate, } from 'react-router-dom'
+import { baseURL } from '../../api/baseUrl';
 
 function Login() {
 const navigate=useNavigate();
@@ -26,7 +27,7 @@ let [formPassword, setPassword] = useState("");
 
   function submitLogin() {
     postRequest(
-        "http://172.16.20.61:5000/user/login",
+      baseURL+"/user/login",
         { username: username, password: formPassword },
         (data) => {
           if (data.accessToken) {
