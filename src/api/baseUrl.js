36da55e8762a6f85cc030e35f30dc132dacee30d
veeranna-ahana <bedasur.veeranna@ahana.co.in>
2024-09-17 11:31:00 +0530

    veer

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,9 +1,11 @@
-// export const baseURL = "http://172.16.20.61:5006";
+/** @format */
 
-// export const baseURL = "http://172.16.20.61:5007"
+// export const baseURL = "http://172.16.20.61:5006";
 
 // export const baseURL = "http://172.16.20.61:5007";
 
-export const baseURL ="http://192.168.1.25:5007"
+// export const baseURL = "http://172.16.20.61:5007";
+//
+export const baseURL = "http://192.168.1.25:4012";
 
 // export const baseURL = "http://localhost:5007"
